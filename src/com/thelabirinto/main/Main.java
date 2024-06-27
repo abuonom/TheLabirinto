a935c2d8fce00e2e357e19b56a100f24e3a05b33
abuonomo <abuonomo@student.42roma.it>
2024-06-27 22:33:55 +0200

    Added some graphics settings

--- src/com/thelabirinto/main/Main.java
+++ src/com/thelabirinto/main/Main.java
@@ -1,7 +1,8 @@
 package com.thelabirinto.main;
+import com.thelabirinto.graphics.Frame;
 
 public class Main {
     public static void main(String[] args) {
-        
+        new Frame();
     }
 }
